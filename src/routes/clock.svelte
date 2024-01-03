65c9351d1cdc918b594dee2d1e2a180fc667ecc4
h4ck0rman <h4ck0rman@gmail.com>
2024-01-03 05:46:14 -0500

    added countdown functionality

---
+++ src/routes/clock.svelte
@@ -0,0 +1,54 @@
+<script>
+    import {onMount, onDestroy} from 'svelte';
+    let pomodoroTime = 25 * 60;
+    let timeElapsed = 0;
+
+    // Start and Stop the timer
+    let running = false;
+    let stateButton = 'start';
+    const toggleState = () => {
+        running = !running;
+        stateButton = running ? 'pause' : 'start'
+    } 
+
+    // Reset the timer 
+    const resetTimer = () => {
+        timeElapsed = 0;
+        running = false
+        stateButton = 'start'
+    }
+
+    // Set up a counter that increments every second
+    let interval = null;
+    onMount(() => {
+		interval = setInterval(() => {
+            if (running) {
+                timeElapsed += 1
+            }
+            
+		}, 1000);
+	});
+    onDestroy(() => {
+		clearInterval(interval);
+	});
+
+    // add prefix zeroes to single digit strings
+    function numPadding (num) {
+        let res = num.toString();
+        while (res.length < 2) res = "0" + res
+        return res
+    }
+
+    $: timeLeft = pomodoroTime - timeElapsed;
+    $: minutes = numPadding(Math.floor(timeLeft / 60));
+    $: seconds = numPadding(Math.floor(timeLeft % 60));
+    
+
+</script>
+
+
+<div >
+    <h1>{minutes}:{seconds}</h1>
+    <button on:click={toggleState}>{stateButton}</button>
+    <button on:click={resetTimer}>reset</button>
+</div>
